5eaa44bcca8e6b1a00b4fed29c4e6834e494a364
Jon Kinney <jonkinney@gmail.com>
2025-04-08 23:42:34 -0500

    feat: scaffold Product model, keep relevant files
    This demo loads things in modals, so we don't need the show or edit
    views, and don't need the .jbuilder files, either.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  resources :products do
+    collection do
+      get :form
+    end
+  end
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
 end
